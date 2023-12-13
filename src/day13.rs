45da2cca2d1df2cc35a91c1e423effcbc5091ec3
seth <seth@finsera.com>
2023-12-13 14:14:17 -0800

    Day 13. Revision.

--- src/day13.rs
+++ src/day13.rs
@@ -25,117 +25,49 @@ fn parse_input(input: impl Iterator<Item = String>) -> Vec<Vec<Vec<char>>> {
     res
 }
 
-fn is_vertical_fold(grid: &Vec<Vec<char>>, col: usize) -> bool {
-    let n_cols = grid[0].len();
-    let n_rows = grid.len();
-    let mut i = col as i64;
-    let mut j = col + 1;
-    while i >= 0 && j < n_cols {
-        if !(0..n_rows).all(|r| grid[r][i as usize] == grid[r][j]) {
-            return false;
-        }
-        i -= 1;
-        j += 1;
-    }
-    true
+fn result_for_diffs(grids: &Vec<Vec<Vec<char>>>, count: usize) -> usize {
+    grids
+        .iter()
+        .map(|grid| {
+            if let Some(col) =
+                (0..grid[0].len() - 1).find(|&col| vertical_diffs(&grid, col) == count)
+            {
+                col + 1
+            } else if let Some(row) =
+                (0..grid.len() - 1).find(|&row| horizontal_diffs(&grid, row) == count)
+            {
+                100 * (row + 1)
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
-fn is_horizontal_fold(grid: &Vec<Vec<char>>, row: usize) -> bool {
+fn vertical_diffs(grid: &Vec<Vec<char>>, col: usize) -> usize {
     let n_cols = grid[0].len();
     let n_rows = grid.len();
-    let mut i = row as i64;
-    let mut j = row + 1;
-    while i >= 0 && j < n_rows {
-        if !(0..n_cols).all(|c| grid[i as usize][c] == grid[j][c]) {
-            return false;
-        }
-        i -= 1;
-        j += 1;
-    }
-    true
+    (0..(col + 1).min(n_cols - col - 1))
+        .flat_map(|d| (0..n_rows).filter(move |&r| grid[r][col - d] != grid[r][col + d + 1]))
+        .count()
+}
+
+fn horizontal_diffs(grid: &Vec<Vec<char>>, row: usize) -> usize {
+    let n_cols = grid[0].len();
+    let n_rows = grid.len();
+    (0..(row + 1).min(n_rows - row - 1))
+        .flat_map(|d| (0..n_cols).filter(move |&c| grid[row - d][c] != grid[row + d + 1][c]))
+        .count()
 }
 
 fn part1(input: impl Iterator<Item = String>) -> usize {
     let grids = parse_input(input);
-    let mut res = 0;
-    for grid in grids {
-        let n_cols = grid[0].len();
-        let n_rows = grid.len();
-        for col in 0..n_cols - 1 {
-            if is_vertical_fold(&grid, col) {
-                println! {"col: {col}"}
-                res += col + 1;
-                continue;
-            }
-        }
-        for row in 0..n_rows - 1 {
-            if is_horizontal_fold(&grid, row) {
-                println! {"row: {row}"}
-                res += 100 * (row + 1);
-                continue;
-            }
-        }
-    }
-    res
-}
-
-fn count_vertical_fold_diffs(grid: &Vec<Vec<char>>, col: usize) -> usize {
-    let n_cols = grid[0].len();
-    let n_rows = grid.len();
-    let mut i = col as i64;
-    let mut j = col + 1;
-    let mut count = 0;
-
-    while i >= 0 && j < n_cols {
-        count += (0..n_rows)
-            .filter(|&r| grid[r][i as usize] != grid[r][j])
-            .count();
-        i -= 1;
-        j += 1;
-    }
-    count
-}
-
-fn count_horizontal_fold_diffs(grid: &Vec<Vec<char>>, row: usize) -> usize {
-    let n_cols = grid[0].len();
-    let n_rows = grid.len();
-    let mut i = row as i64;
-    let mut j = row + 1;
-    let mut count = 0;
-
-    while i >= 0 && j < n_rows {
-        count += (0..n_cols)
-            .filter(|&c| grid[i as usize][c] != grid[j][c])
-            .count();
-        i -= 1;
-        j += 1;
-    }
-    count
+    result_for_diffs(&grids, 0)
 }
 
 fn part2(input: impl Iterator<Item = String>) -> usize {
     let grids = parse_input(input);
-    let mut res = 0;
-    for grid in grids {
-        let n_cols = grid[0].len();
-        let n_rows = grid.len();
-        for col in 0..n_cols - 1 {
-            let count = count_vertical_fold_diffs(&grid, col);
-            if count == 1 {
-                res += col + 1;
-                continue;
-            }
-        }
-        for row in 0..n_rows - 1 {
-            let count = count_horizontal_fold_diffs(&grid, row);
-            if count == 1 {
-                res += 100 * (row + 1);
-                continue;
-            }
-        }
-    }
-    println!();
-    res
+    result_for_diffs(&grids, 1)
 }
 
 #[cfg(test)]
